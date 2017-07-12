1d521481e414b2cd313ed0bd969055ce9abb03f7
Mini_Moon <972242736@qq.com>
2017-07-12 18:09:16 +0800

    图片上传

--- src/main/java/com/mmf/web/FileController.java
+++ src/main/java/com/mmf/web/FileController.java
@@ -1,5 +1,6 @@
 package com.mmf.web;
 
+import com.mmf.dto.ResponseModel;
 import com.mmf.model.User;
 import com.mmf.service.IFileService;
 import org.springframework.stereotype.Controller;
@@ -28,47 +29,144 @@ public class FileController {
     @ResponseBody
 //    private Object uploadFile(@RequestParam(value = "data",defaultValue = "",required = false) String data,@RequestParam(value = "params",defaultValue = "",required = false) Map<String, MultipartFile> params) {
 //    private Object uploadFile(@RequestParam(value = "data",defaultValue = "",required = false) String data,Map<String, MultipartFile> params) {
-    private Object uploadFile(@RequestPart(name = "data",value = "data",required = false) String data,@RequestBody Map<String, RequestBody> params) {
+    private Object uploadFile(@RequestPart(name = "data", value = "data", required = false) String data, @RequestBody Map<String, RequestBody> params) {
         List<String> list = new ArrayList<String>();
         list.add("sss");
 //                = fileService.uploadFile();
 //        model.addAttribute("list", list);
         // list.jsp + model = ModelAndView
-        System.out.println("**********/////////" +data);
+        System.out.println("**********/////////" + data);
         return list;
     }
-    @RequestMapping(value = "/upload", method = RequestMethod.POST)
-    @ResponseBody
-        public Object uploadFile(HttpServletRequest request) throws IllegalStateException, IOException
-        {
-            long  startTime=System.currentTimeMillis();
-            //将当前上下文初始化给  CommonsMutipartResolver （多部分解析器）
-            CommonsMultipartResolver multipartResolver=new CommonsMultipartResolver(
-                    request.getSession().getServletContext());
-            //检查form中是否有enctype="multipart/form-data"
-            if(multipartResolver.isMultipart(request))
-            {
-                //将request变成多部分request
-                MultipartHttpServletRequest multiRequest=(MultipartHttpServletRequest)request;
-                //获取multiRequest 中所有的文件名
-                Iterator iter=multiRequest.getFileNames();
 
-                while(iter.hasNext())
-                {
-                    //一次遍历所有文件
-                    MultipartFile file=multiRequest.getFile(iter.next().toString());
-                    if(file!=null)
-                    {
-                        String path= "D:/upload/"+file.getOriginalFilename();
+    //
+    @RequestMapping(value = "/upload1", method = RequestMethod.POST)
+    @ResponseBody
+    public Object uploadFile(@RequestParam Map<String,MultipartFile> files1,HttpServletRequest request) throws IllegalStateException, IOException {
+        long startTime = System.currentTimeMillis();
+        //将当前上下文初始化给  CommonsMutipartResolver （多部分解析器）
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+                request.getSession().getServletContext());
+        //检查form中是否有enctype="multipart/form-data"
+        if (multipartResolver.isMultipart(request)) {
+            //将request变成多部分request
+            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+            //获取multiRequest 中所有的文件名
+            Iterator iter = multiRequest.getFileNames();
+
+            while (iter.hasNext()) {
+                //一次遍历所有文件
+                List<MultipartFile> files = multiRequest.getFiles(iter.next().toString());
+                (new File("D:/upload")).mkdirs();
+                if (files != null && files.size() > 0) {
+                    for (MultipartFile file : files) {
+                        String path = "D:/upload/" + file.getOriginalFilename();
                         //上传
                         file.transferTo(new File(path));
                     }
-
                 }
 
             }
-            long  endTime=System.currentTimeMillis();
-            System.out.println("方法三的运行时间："+String.valueOf(endTime-startTime)+"ms");
-            return "list";
+
         }
+        long endTime = System.currentTimeMillis();
+        System.out.println("方法三的运行时间：" + String.valueOf(endTime - startTime) + "ms");
+        ResponseModel model = new ResponseModel(0, "获取数据成功");
+        List<String> list = new ArrayList<String>();
+        list.add("哈哈");
+        model.setData(list);
+        return model;
+    }
+
+    //    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+//    @ResponseBody
+//    public Object uploadFile(@RequestParam Map<String,MultipartFile> files) throws IllegalStateException, IOException {
+//        long startTime = System.currentTimeMillis();
+//        //将当前上下文初始化给  CommonsMutipartResolver （多部分解析器）
+////        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+////                request.getSession().getServletContext());
+////        //检查form中是否有enctype="multipart/form-data"
+////        if (multipartResolver.isMultipart(request)) {
+////            //将request变成多部分request
+////            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+////            //获取multiRequest 中所有的文件名
+////            Iterator iter = multiRequest.getFileNames();
+//
+////            while (iter.hasNext()) {
+////                //一次遍历所有文件
+//
+//        (new File("D:/upload")).mkdirs();
+////        Set<String> keySet = files.keySet();
+//
+////        for (Iterator<String> iterator = keySet.iterator(); iterator.hasNext(); ) {
+////            String key = iterator.next();
+//
+////            String value = (String)files.get(key);
+////            CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+////                request.getSession().getServletContext());
+////        //检查form中是否有enctype="multipart/form-data"
+////        if (multipartResolver.isMultipart(request)) {
+////            //将request变成多部分request
+////            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+////            //获取multiRequest 中所有的文件名
+////            Iterator iter = multiRequest.getFileNames();
+////            /                    for (MultipartFile file : files) {
+////            String path = "D:/upload/" + value.getOriginalFilename();
+////            //上传
+////            value.transferTo(new File(path));
+////            System.out.println("value"+value);
+////
+////        }
+//////                List<MultipartFile> files = multiRequest.getFiles(iter.next().toString());
+////                if (files != null && files.length > 0) {
+//
+////                    }
+////                }
+//
+////            }
+//
+////        }
+//        long endTime = System.currentTimeMillis();
+//        System.out.println("方法三的运行时间：" + String.valueOf(endTime - startTime) + "ms");
+//        ResponseModel model = new ResponseModel(0, "获取数据成功");
+//        List<String> list = new ArrayList<String>();
+//        list.add("哈哈");
+//        model.setData(list);
+//        return model;
+//    }
+    @RequestMapping(value = "/upload", method = RequestMethod.POST)
+    @ResponseBody
+    public Object uploadFile1(MultipartHttpServletRequest request) throws IllegalStateException, IOException {
+        long startTime = System.currentTimeMillis();
+//        List<MultipartFile> files= request.getFiles("files");
+        //将当前上下文初始化给  CommonsMutipartResolver （多部分解析器）
+//        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver(
+//                request.getSession().getServletContext());
+//        //检查form中是否有enctype="multipart/form-data"
+//        if (multipartResolver.isMultipart(request)) {
+//            //将request变成多部分request
+//            MultipartHttpServletRequest multiRequest = (MultipartHttpServletRequest) request;
+//            //获取multiRequest 中所有的文件名
+        Iterator iter = request.getFileNames();
+
+        (new File("D:/upload")).mkdirs();
+        while (iter.hasNext()) {
+            //一次遍历所有文件
+            List<MultipartFile> files = request.getFiles(iter.next().toString());
+            if (files != null && files.size() > 0) {
+                for (MultipartFile file : files) {
+                    String path = "D:/upload/" + file.getOriginalFilename();
+                    //上传
+                    file.transferTo(new File(path));
+                }
+            }
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("方法三的运行时间：" + String.valueOf(endTime - startTime) + "ms");
+        ResponseModel model = new ResponseModel(0, "获取数据成功");
+        List<String> list = new ArrayList<String>();
+        list.add("哈哈");
+        model.setData(list);
+        return model;
+    }
 }
--- src/main/java/com/mmf/web/UserController.java
+++ src/main/java/com/mmf/web/UserController.java
@@ -5,12 +5,11 @@ import com.mmf.model.User;
 import com.mmf.service.IUserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by MMF on 2017/6/14.
@@ -27,9 +26,18 @@ public class UserController {
         List<User> list = userService.getUserList(name);
 //        model.addAttribute("list", list);
         // list.jsp + model = ModelAndView
-        ResponseModel model = new ResponseModel(0,"获取数据成功");
+        ResponseModel model = new ResponseModel<List<User>>(0, "获取数据成功");
         model.setData(list);
         System.out.println("**********" + list.toString());
+        return model;
+    }
+
+    @RequestMapping(value = "/list1", method = RequestMethod.GET)
+    @ResponseBody
+    private Object list(@RequestParam Map<String, Object> params) {
+        List<User> list = userService.getUserList(params.get("mao").toString());
+        ResponseModel model = new ResponseModel<List<User>>(0, "获取数据成功");
+        model.setData(list);
         return model;
     }
 }
