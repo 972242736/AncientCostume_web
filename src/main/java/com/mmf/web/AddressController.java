9e6c7bb701f4d5341cd9d05e38d357ce5e4fb6e2
Mini_Moon <972242736@qq.com>
2017-09-28 17:43:53 +0800

    添加获取地址

---
+++ src/main/java/com/mmf/web/AddressController.java
@@ -0,0 +1,12 @@
+package com.mmf.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by MMF on 2017-09-28.
+ */
+@Controller
+@RequestMapping("/address")
+public class AddressController {
+}
